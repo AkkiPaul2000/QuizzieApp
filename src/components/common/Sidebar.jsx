35b38783feee31a841c08e20fbbdbf171316bd05
akkipaul2000 <skypaul2000@gmail.com>
2024-08-23 12:09:13 +0530

    small changes on sidebar

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,11 +1,12 @@
 // src/components/common/Sidebar.jsx
 import React, { useState } from 'react';
-import { Link, Navigate } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import './Common.css'
 import { useAuth } from '../../utils/auth';
 import { toast } from 'react-toastify';
 const Sidebar = () => {
   const { logout } = useAuth();
+  const navigate=useNavigate();
   const handleLogout = async (e) => {
     logout()
     toast.dark('Logout successful!');
@@ -15,13 +16,19 @@ const Sidebar = () => {
   return (
     <aside style={{ width: '200px', backgroundColor: '#FFFFFF', color: '#474444', padding: '0px 20px' }}>
       <h2 className='brandName'>QUIZZIE</h2>
-      <nav>
-        <ul style={{ listStyleType: 'none', padding: 0 }}>
-          <li><Link to="/dashboard" className={selIndex==0?'activeLink':''} onClick={()=>setSelIndex(0)}>Dashboard</Link></li>
-          <li><Link to="/createQuiz" className={selIndex==1?'activeLink':''} onClick={()=>setSelIndex(1)}>Create Quiz</Link></li>
-          <li><Link to="/analytics" className={selIndex==2?'activeLink':''} onClick={()=>setSelIndex(2)}>Quiz Analytics</Link></li>
-        </ul>
-      </nav>
+      
+      <div className='navButtons'>
+        <div className={`navButton ${selIndex == 0 ? 'activeLink' : ''}`} onClick={()=>{setSelIndex(0)
+          navigate('/dashboard')
+        }}>Dashboard</div>
+        <div className={`navButton ${selIndex == 1 ? 'activeLink' : ''}`} onClick={()=>{setSelIndex(1)
+          navigate('/analytics')
+        }}>Analytics</div>
+        <div className={`navButton ${selIndex == 2 ? 'activeLink' : ''}`} onClick={()=>{setSelIndex(2)
+          navigate('/createQuiz')
+        }}>Create Quiz</div>
+
+      </div>
       <div className='logOut' onClick={handleLogout}>LOGOUT</div>
     </aside>
   );
