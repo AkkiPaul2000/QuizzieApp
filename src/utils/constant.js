f4e2b3a54bc4bd859860ba8fd94823fbbee39423
Akki <skypaul2000@gmail.com>
2024-08-27 20:50:48 +0530

    small changes on frontend URL

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,6 @@
 export const BACKEND_URL = 'https://quizzie-fxik.onrender.com';
 // export const BACKEND_URL = 'http://localhost:5000';
 
-export const FRONTEND_URL = 'http://localhost:5173/quiz/'
+// export const FRONTEND_URL = 'http://localhost:5173/quiz/'
+
+export const FRONTEND_URL = 'https://quizzie-xi.vercel.app/quiz'
